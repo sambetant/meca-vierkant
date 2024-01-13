4db0b1d3267829e1010ca72b5f7cd9c3a5e61cc6
sambetant <104916257+sambetant@users.noreply.github.com>
2024-01-13 22:39:56 +0100

    pxt.json, main.blocks, main.ts bijwerken

--- main.ts
+++ main.ts
@@ -1,3 +1,11 @@
+function Vooruit () {
+    motor.MotorRun(motor.Motors.M1, motor.Dir.CW, Speed)
+    motor.MotorRun(motor.Motors.M2, motor.Dir.CW, Speed)
+    motor.MotorRun(motor.Motors.M3, motor.Dir.CW, Speed)
+    motor.MotorRun(motor.Motors.M4, motor.Dir.CW, Speed)
+}
+let Speed = 0
+Speed = 300
 basic.forever(function () {
-	
+    Vooruit()
 })
